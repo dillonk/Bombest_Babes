5445fe8cf8dedc1f7c02c0f89565f975bff7fecc
Dillon Kissoon <delayn15@gmail.com>
2012-12-02 22:28:39 -0500

    Updated directory

--- site-wide/functions/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
- session_start();
- unset($_SESSION['u_name']);
- session_destroy();
- echo '<script type="text/javascript">var t=setTimeout(window.location.href="../../../index.html",3000);</script>';
- ?>
